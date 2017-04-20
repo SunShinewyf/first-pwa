a3cacc6d99bb01b01d00ee744eb7e1c99b33da7e
SunShinewyf <2632807692@qq.com>
2017-04-20 20:38:41 +0800

    add the readme.md and worker.js

---
+++ README.md
@@ -0,0 +1,19 @@
+# a pwa demo of pp android
+
+## what we need
+> Chrome 52 or above
+> [Web Server for Chrome](https://chrome.google.com/webstore/detail/web-server-for-chrome/ofhbbkphhbklhfoeikjpcbhemlocgigb)
+
+##run
+
+```shell
+ git clone https://github.com/SunShinewyf/first-pwa.git
+```
+
+open the [url](https://chrome.google.com/webstore/detail/web-server-for-chrome/ofhbbkphhbklhfoeikjpcbhemlocgigb?hl=en) and click the blue button to Launch App
+
+click "CHOOSE FOLDER" as follow
+![Alt text](./1492685792655.png)
+
+then  click the url
+![Alt text](./1492685875856.png)
---
+++ worker.js
@@ -0,0 +1,50 @@
+/**
+ * author:SunShinewyf
+ * date:2017-4-20
+ * desc:some setting of worker
+ **/
+var dataCacheName = 'ppData-v1';
+var cacheName = 'ppPWA-final-1';
+
+var filesToCache = [
+    '/',
+    '/index.html',
+    'styles/index.css'
+];
+
+self.addEventListener('install',function(e){
+    console.log('[pp serviceWorker install');
+    e.waitUntil(
+        caches.keys().then(function(keyList){
+            return Promise.all(keyList.map(function(key){
+                if(key!==cacheName && key!==dataCacheName){
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }))
+        })
+    );
+    return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function(e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+    var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        e.respondWith(
+            caches.open(dataCacheName).then(function(cache) {
+                return fetch(e.request).then(function(response){
+                    cache.put(e.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    } else {
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
+});
