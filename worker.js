bb700d5be5faeb36d97310752ecb8821ab7cec92
SunShinewyf <2632807692@qq.com>
2017-04-21 17:39:11 +0800

    change the worker.js

--- worker.js
+++ worker.js
@@ -15,18 +15,26 @@ var filesToCache = [
 self.addEventListener('install',function(e){
     console.log('[pp serviceWorker install');
     e.waitUntil(
-        caches.keys().then(function(keyList){
-            return Promise.all(keyList.map(function(key){
-                if(key!==cacheName && key!==dataCacheName){
-                    console.log('[ServiceWorker] Removing old cache', key);
-                    return caches.delete(key);
-                }
-            }))
+        caches.open(cacheName).then(function(cache){
+            console.log('serviceWorker Caching app shell');
+            return cache.addAll(filesToCache);
         })
     );
     return self.clients.claim();
 });
 
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
 
 self.addEventListener('fetch', function(e) {
     console.log('[Service Worker] Fetch', e.request.url);
